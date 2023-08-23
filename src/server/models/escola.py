7c30b0f2da50e10bda166395ebf7daf9c64ee393
Isac Kaik <isackaik1@gmail.com>
2023-08-23 18:49:44 -0300

    Iniciado documentação da entidade escola

---
+++ src/server/models/escola.py
@@ -0,0 +1,17 @@
+from flask_restx import fields
+from server.instance import server
+
+escola = server.api('Escola', {
+
+    'inep': fields.Integer(description='Código da instituição de ensino da escola', required=True),
+    'nome': fields.String(description='Nome da escola', required=True),
+    'ano_entrada': fields.String(description='Ano em que a escola deu entrada no projeto', requred=True),
+    'status_escola': fields.String(description='Estado em que a escola se encontra', required=True, max=1),
+    'tipo': fields.String(description='Tipo da escola: 1 - urbana, 2 - rural', required=True),
+    'dependencia': fields.String(description='Tipo de dependência da escola', required=True),
+    'latitude': fields.Decimal(description='Latitude da escola'),
+    'longitude': fields.Decimal(description='Longitude da escola'),
+    'endereco': fields.String(description='Endereço da escola', max=300),
+    'id_rede': fields.Integer(description='identificador da rede em que a escola está associada', required=True)
+
+})
