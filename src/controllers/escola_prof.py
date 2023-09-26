3751cb33712cdbf44f2fd3465820a6848cab17c1
Isac Kaik <isackaik1@gmail.com>
2023-09-26 20:46:17 -0300

    Create function for insert escolas_prof

--- src/controllers/escola_prof.py
+++ src/controllers/escola_prof.py
@@ -10,10 +10,29 @@ CREATE_ESCOLA_PROF = '''INSERT INTO projeto_educacional.escolas_prof (inep_escol
                         matutino, vespertino, noturno) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'''
 READ_ESCOLA_PROF = 'SELECT * FROM projeto_educacional.escolas_prof WHERE cpf_professor = %s AND inep_escola = %s'
 DELETE_ESCOLA_PROF = 'DELETE FROM projeto_educacional.escolas_prof WHERE cpf_professor = %s AND inep_escola = %s'
-UPDATE_ESCOLA_PROF = '''UPDATE projeto_educacional.escolas_prof SET vinculo = %s, usuario_plataforma = %s, ch_trabalho = %s
+SQL_UPDATE = '''UPDATE projeto_educacional.escolas_prof SET vinculo = %s, usuario_plataforma = %s, ch_trabalho = %s
                         matutino = %s, vespertino = %s, noturno = %s WHERE cpf_professor = %s AND inep_escola = %s'''
 
 @api.route('/escola_prof')
 class Escola_Prof(Resource):
-    def create_escola_prof(self):
+    def post(self):
         dados = request.json()
+        inep_escola = dados.get('inep_escola')
+        cpf_professor = dados.get('cpf_professor')
+        vinculo = dados.get('vinculo')
+        usuario_plataforma = dados.get('usuario_plataforma')
+        ch_trabalho = dados.get('ch_trabalho')
+        matutino = dados.get('matutino')
+        vespertino = dados.get('vespertino')
+        noturno = dados.get('noturno')
+        try:
+            with connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(CREATE_ESCOLA_PROF, (inep_escola, cpf_professor, vinculo, usuario_plataforma, ch_trabalho, matutino, vespertino, noturno))
+            return f'Escola_prof cadastrada'
+        except psycopg2.IntegrityError as e:
+            return f'Erro de integridade: {e}'
+        except psycopg2.Error as e:
+            return f'Erro no banco de dados: {e}'
+        except Exception as e:
+            return f'Erro inesperado: {e}'
